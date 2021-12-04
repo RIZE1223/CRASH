cf508b35ae75d921567e24667e383690ec0a22ac
handa <ryou10121012@icloud.com>
2021-12-04 16:47:40 +0900

    吉田君へ：ひとまず時間表示の方はこっちで直しました できれば時間の表示の仕方を　分：秒　って感じにお願いします

--- CRASH/Assets/TimeCount.cs
+++ CRASH/Assets/TimeCount.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class TimeCount : MonoBehaviour
 {
     //カウントダウン
-    public float countdown = 5.0f;
+    public float countdown = 10.0f;
 
     // 資金の処理
     public float time2 = 0.0f;
@@ -24,8 +24,13 @@ public class TimeCount : MonoBehaviour
         time2 += Time.deltaTime;
 
         //時間と資金を表示する
-        timeText.text = countdown.ToString("f1") + "秒\n" + "資金：" + money.ToString("f1");
-
+        timeText.text = countdown.ToString("f0") + "秒\n" + "資金：" + money.ToString("f0");
+        /*
+         f0 = 小数点を表示しない
+         f1 = 小数点第一位まで表示
+         f2 = 小数点第二位まで表示
+           ....
+         */
 
         //countdownが0以下になったとき
         if (countdown <= 0)
