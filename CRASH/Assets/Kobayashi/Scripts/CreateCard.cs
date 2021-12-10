5e60f5a9431a8211f08d7dcf0a2cf15922035d52
tyobo <cchobo32@gmail.com>
2021-12-10 16:04:25 +0900

    手元に５枚生成、リロードボタンを押すと余った手札を破棄して新しい手札を５枚生成

--- CRASH/Assets/Kobayashi/Scripts/CreateCard.cs
+++ CRASH/Assets/Kobayashi/Scripts/CreateCard.cs
@@ -7,15 +7,37 @@ public class CreateCard : MonoBehaviour
     [SerializeField] private Transform PlayerHand;  //場所参照
     [SerializeField] private List<GameObject> cards = new List<GameObject>();  //カードリスト作成
 
-    private void FixedUpdate()
+    private void Start()
+    {
+        Create();
+    }
+    /// <summary>
+    /// カードを生成する
+    /// </summary>
+    public void Create()
     {
 
         int ObjCount = this.transform.childCount;
-        int num = Random.Range(0, cards.Count); //0からカードのリストの数だけランダムでだす
 
-        if (ObjCount <= 5)
-        {      
-                GameObject.Instantiate(cards[num], PlayerHand);    //カード生成     
+        if (ObjCount <= 0)
+        {
+            for (int i = 0; i <= 4; i++)
+            {
+                int num = Random.Range(0, cards.Count); //0からカードのリストの数だけランダムでだす
+                GameObject.Instantiate(cards[num], PlayerHand);    //カード生成
+            }
+        }
+        else
+        {
+            foreach (Transform n in PlayerHand.transform)   //子にあるオブジェクト全消し
+            {
+                GameObject.Destroy(n.gameObject);
+            }
+            for (int i = 0; i <= 4; i++)
+            {
+                int num = Random.Range(0, cards.Count); //0からカードのリストの数だけランダムでだす
+                GameObject.Instantiate(cards[num], PlayerHand);    //カード生成
+            }
         }
     }
 }
